43829c2c5c021b751e4d4643daff3126d9155a68
Salman Ahmed <emailme@ssahmed.co.uk>
2015-10-09 00:05:26 +0000

    Removed processPayment from index.php

--- index.php
+++ index.php
@@ -26,8 +26,6 @@ if (preg_match('/^\/post_payment/', $method)) {
     $flypay = new \App\FlyPay($data);
     $flypay->postPayment();
 
-    //processPayment($data);
-
 } else if (preg_match('/^\/get_payments_report/', $method)) {
     getPaymentsReport($data);
 
@@ -39,76 +37,3 @@ if (preg_match('/^\/post_payment/', $method)) {
 // Do authentication
 
 /* TO DO: Authentication */
-
-
-############# Functions ############
-
-/**
- * Process payment
- */
-function processPayment($data = array())
-{
-    $return = [];
-
-    $required_fields = [
-        'payment_amount_ex_vat',        // payment total excluding VAT
-        'vat_amount',                   // total VAT
-        'gratuity_amount',              // tip
-        'table_id',                     // table number
-        'branch_id',                    // the ID of the restaurant location where the table is at
-        'payment_vendor_id',            // a payment reference for the third party (who created the payment)
-        'payment_taken_at',             // timestamp provided in the request
-        'employee_id',                  // who served the customer
-        'order_id',                     // unique order reference
-        'terminal_id',                  // payment terminal used
-    ];
-
-    // Validate payment request
-    foreach ($required_fields as $f) {
-        if (!isset($data[$f])) {
-            $return['success'] = 0;
-            $return['message'][] = 'Invalid Payment Request. Missing: ' . $f;
-
-            return output($return);
-        }
-    }
-
-    // Validation passed -> Create new payment
-
-    $payment = new Payment();
-    $payment->payment_amount_ex_vat = $data['payment_amount_ex_vat'];
-    $payment->vat_amount = $data['vat_amount'];
-    $payment->gratuity_amount = (isset($data['gratuity_amount'])) ? $data['gratuity_amount'] : 0;
-    $payment->branch_id = $data['branch_id'];
-    $payment->payment_vendor_id = $data['payment_vendor_id'];
-    $payment->employee_id = $data['employee_id'];
-    $payment->order_id = $data['order_id'];
-    $payment->payment_taken_at = \Carbon\Carbon::createFromFormat('Y-m-d H:i', $data['payment_taken_at']);
-    $payment->terminal_id = $data['terminal_id'];
-    $payment->created_at = \Carbon\Carbon::now();
-
-    // Save it into the database
-    if ($payment->save()) {
-        $return['success'] = 1;
-        $return['payment_id'] = $payment->id;
-
-        output($return);
-    }
-
-}
-
-
-/**
- * Return report
- */
-function getPaymentsReport()
-{
-    echo "get payments report function";
-}
-
-function output($output = array())
-{
-    header('Content-Type: application/json');
-
-    echo json_encode($output);
-}
